88367a6716390e6af18041e2040b9244b5b2684e
OwlCodR <max.lyovkin@mail.ru>
2023-11-04 19:21:33 +0300

    Small fix

--- src/api.py
+++ src/api.py
@@ -2,6 +2,7 @@ import json
 import requests
 import os
 import logging
+import asyncio
 
 from flask import Flask, request, jsonify
 
@@ -16,8 +17,8 @@ logging.getLogger(__name__).setLevel(logging.DEBUG)
 ACCESS_SERVICE_BASE_URL_KEY = 'ACCESS_SERVICE_BASE_URL'
 
 
-def __notifyUsers(users: list, message: str):
-    Bot.notifyUsers(users=users, message=message)
+async def __notifyUsers(users: list, message: str):
+    await Bot.notifyUsers(users=users, message=message)
 
 
 def __getUsersToNotify(token: str) -> list:
@@ -50,7 +51,7 @@ def __parseRequest(request) -> (str, str):
 def alert():
     error_message, token = __parseRequest(request)
     users = __getUsersToNotify(token)
-    __notifyUsers(users, error_message)
+    asyncio.run(__notifyUsers(users, error_message))
     return ('', 200)
 
 
