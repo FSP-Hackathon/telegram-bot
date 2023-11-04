3d300071859ef5a6191206159d8f4eed0cca8adb
OwlCodR <max.lyovkin@mail.ru>
2023-11-04 19:34:03 +0300

    Fix

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 from api import runApi
 from bot import Bot
-
+from multiprocessing import Process
 
 def main() -> None:
-    Bot.runBot()
-    runApi()
-    
+    api = Process(target=runApi())
+    bot = Process(target=Bot.runBot())
+    api.start()
+    bot.start()    
 
 if __name__ == '__main__':
     main()
