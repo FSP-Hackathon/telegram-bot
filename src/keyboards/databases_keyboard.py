63f3a9ad34df2ae1b529f56239883d92a26e8ea9
OwlCodR <max.lyovkin@mail.ru>
2023-11-05 07:47:44 +0300

    :construction: Work in progress

--- src/keyboards/databases_keyboard.py
+++ src/keyboards/databases_keyboard.py
@@ -2,10 +2,11 @@ from telebot import types
 
 from constants.strings import Strings
 
-
 def databasesKeyboard(names: list[str]):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
     for name in names:
         button = types.KeyboardButton(name)
         markup.add(button)
+    
+    return markup
