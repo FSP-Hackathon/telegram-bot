fa8a57aadc14835cfd4874c1e9d9aa1ae1ff0127
OwlCodR <max.lyovkin@mail.ru>
2023-11-05 08:54:10 +0300

    :construction: Work in progress

--- src/keyboards/shortcuts_keyboard.py
+++ src/keyboards/shortcuts_keyboard.py
@@ -4,7 +4,7 @@ from constants.strings import Strings
 
 
 def shortcutsKeyboard():
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=1)
 
     shortcut_restart = types.KeyboardButton(
         text=Strings.translate('shortcut_restart'),
